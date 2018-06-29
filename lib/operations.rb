b608b2a1d04e7bc2e9a07170287d83a1f972d31e
Scott Tang <scott.q.tang@gmail.com>
2018-06-29 14:46:58 -0500

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,22 @@
 def unsafe?(speed)
-
+    if (speed >= 40 && speed <= 60)
+        false
+    else
+        true
+    end
 end
 
 
 
 def not_safe?(speed)
+    (speed >= 40 && speed <= 60) ? false : true
 	
 end
 	
+puts unsafe?(40)
+puts unsafe?(45)
+puts unsafe?(60)
+puts unsafe?(61)
+puts unsafe?(39)
 
 
